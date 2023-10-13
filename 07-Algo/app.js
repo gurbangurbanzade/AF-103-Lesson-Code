b2c2356856a79bc9a3d6a995411c477407fc0a32
gurbangurbanzade <gurban.gurbanzade@gmail.com>
2023-10-13 12:51:04 +0400

    algo added

---
+++ 07-Algo/app.js
@@ -0,0 +1,77 @@
+// Verilmiş ədədin rəqəmlərinin cəmini, hasilini və ədədi ortasını tapan proqram tərtib edin.
+// let eded = "1234";
+// let cem = 0;
+// let hasil = 1;
+
+// for (let i = 0; i < eded.length; i++) {
+//   //   console.log(eded[i]);
+//   cem += +eded[i]; // NUmber(eded)
+//   hasil *= +eded[i];
+// }
+// console.log("cem", cem);
+// console.log("hasil", hasil);
+// console.log("ededi orta", cem / +eded.length);
+
+// 6. Verilmiş ədədin bütün bölənlərini tapan proqram tərtib edin.
+// let eded = "1211234567";
+
+// for (let i = 1; i <= eded; i++) {
+//   if (eded % i == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 7. Verilmiş ədədin bölənlərinin sayını tapan proqram tərtib edin.
+
+// let eded = "12";
+// let saygac = 0;
+
+// for (let i = 1; i <= eded; i++) {
+//   if (eded % i == 0) {
+//     console.log(i);
+//     saygac++; // saygac= saygac +1
+//   }
+// }
+
+// console.log("saygac", saygac);
+
+//    let arr=[3,8,2,5,4,10,7,6]
+// 8. Verilmiş array-in tək elementlərinin indeksini çapa verən proqram tərtib edin.
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 1) {
+//     // console.log(arr[i]);
+//     // console.log(i);
+
+//     console.log(`${arr[i]} elementinin indeksi = ${i}`);
+//   }
+// }
+
+// 9. Verilmiş array-in tək indeksli elementlərini çapa verən proqram tərtib edin.
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 2 == 1) {
+//     // console.log(arr[i]);
+//     // console.log(i);
+//     console.log(`${arr[i]} elementinin indeksi = ${i}`);
+//   }
+// }
+
+// 10. Verilmiş array-in max elementini çapa verən proqram tərtib edin.
+// let arr = [3, 8, 2, 5, 4, 99, 10, 7, 6];
+// let max = arr[0];
+
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//     // console.log(arr[i]);
+//     // console.log(i);
+//   }
+// }
+
+// console.log("max=", max);
+
+// 11. Verilmiş array-in cüt elementlərinin max elementini çapa verən proqram tərtib edin.
+
+// 12. Verilmiş array-in min elementinin indeksini çapa verən proqram tərtib edin.
